5d7abe7abeb97258ef170d4d91f8e568824ab621
Yuke Zhu <zyk0902@gmail.com>
2018-10-26 00:53:25 -0700

    make demonstration util as optional imports

--- robosuite/__init__.py
+++ robosuite/__init__.py
@@ -8,7 +8,6 @@ from robosuite.environments.sawyer_nut_assembly import SawyerNutAssembly
 
 from robosuite.environments.baxter_lift import BaxterLift
 from robosuite.environments.baxter_peg_in_hole import BaxterPegInHole
-from robosuite.wrappers import DataCollectionWrapper
 
 __version__ = "0.1.0"
 __logo__ = """
