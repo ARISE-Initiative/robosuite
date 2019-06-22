81434ffad9e1ab50f250fe5eee1fab2dbae4dc6a
Andrew Kondrich <andrewk1@stanford.edu>
2019-06-22 11:34:18 -0700

    Add Domain Randomization Wrapper implementation

--- robosuite/wrappers/__init__.py
+++ robosuite/wrappers/__init__.py
@@ -2,6 +2,7 @@ from robosuite.wrappers.wrapper import Wrapper
 from robosuite.wrappers.ik_wrapper import IKWrapper
 from robosuite.wrappers.data_collection_wrapper import DataCollectionWrapper
 from robosuite.wrappers.demo_sampler_wrapper import DemoSamplerWrapper
+from robosuite.wrappers.domain_randomization_wrapper import DRWrapper
 
 try:
     from robosuite.wrappers.gym_wrapper import GymWrapper
--- robosuite/wrappers/domain_randomization_wrapper.py
+++ robosuite/wrappers/domain_randomization_wrapper.py
@@ -4,9 +4,26 @@ robosuite environments.
 """
 
 from robosuite.wrappers import Wrapper
+from robosuite.utils.mjmod import TextureModder
+
 
 class DRWrapper(Wrapper):
     env = None
 
     def __init__(self, env):
-        super().__init__(env=env)
+        super().__init__(env)
+        # TODO: Do not hardcode SawyerLift env update
+        cube_geom = self.env.model.worldbody.findall("./body/[@name='cube']/geom")[0]
+        cube_geom.set('material', 'arm_mat')
+        self.modded_xml = self.env.model.get_xml()
+        self.reset()
+
+    def reset(self):
+        self.env.reset_from_xml_string(self.modded_xml)
+        self.modder = TextureModder(self.env.sim)  # modder has to access updated model
+        self.modder.randomize()
+
+    def render(self, **kwargs):
+        self.modder.randomize()
+        super().render(**kwargs)
+
