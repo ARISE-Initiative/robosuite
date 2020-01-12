069f099ac9ecb7f9d833e7ad22b232d49614b465
josiah_wong <84cremebrule@gmail.com>
2020-01-11 20:37:46 -0800

    removed vices gripper

--- robosuite/models/grippers/gripper_factory.py
+++ robosuite/models/grippers/gripper_factory.py
@@ -7,7 +7,6 @@ from .robotiq_gripper import RobotiqGripper
 from .pushing_gripper import PushingGripper
 from .robotiq_three_finger_gripper import RobotiqThreeFingerGripper
 from .panda_gripper import PandaGripper
-from .panda_vices_gripper import PandaVICESGripper
 
 
 def gripper_factory(name):
@@ -39,6 +38,4 @@ def gripper_factory(name):
         return RobotiqThreeFingerGripper()
     if name == "PandaGripper":
         return PandaGripper()
-    if name == "PandaVICESGripper":
-        return PandaVICESGripper()
-    raise ValueError("Unkown gripper name {}".format(name))
+    raise ValueError("Unknown gripper name {}".format(name))
