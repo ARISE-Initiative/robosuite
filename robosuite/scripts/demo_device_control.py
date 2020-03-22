7a15f568cdd79b0f38c715f99e7552cc6cc7213e
amandlek <ajay.mandlekar@gmail.com>
2020-03-22 11:43:46 -0700

    make keyboard default for demo device script

--- robosuite/scripts/demo_device_control.py
+++ robosuite/scripts/demo_device_control.py
@@ -111,7 +111,7 @@ if __name__ == "__main__":
     parser.add_argument("--arm", type=str, default="right", help="Which arm to control (eg bimanual) 'right' or 'left'")
     parser.add_argument("--switch-on-click", action="store_true", help="Switch gripper control on gripper click")
     parser.add_argument("--controller", type=str, default="osc", help="Choice of controller. Can be 'ik' or 'osc'")
-    parser.add_argument("--device", type=str, default="spacemouse")
+    parser.add_argument("--device", type=str, default="keyboard")
     parser.add_argument("--pos-sensitivity", type=float, default=1.5, help="How much to scale position user inputs")
     parser.add_argument("--rot-sensitivity", type=float, default=1.5, help="How much to scale rotation user inputs")
     args = parser.parse_args()
