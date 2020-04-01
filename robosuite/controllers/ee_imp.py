27285d3282c01060b1374c34b1f7c375fd709816
amandlek <ajay.mandlekar@gmail.com>
2020-04-01 16:25:25 -0700

    fix subtle issue with output bounds

--- robosuite/controllers/ee_imp.py
+++ robosuite/controllers/ee_imp.py
@@ -2,6 +2,7 @@ from robosuite.controllers.base_controller import Controller
 from robosuite.utils.control_utils import *
 import robosuite.utils.transform_utils as T
 import numpy as np
+from collections.abc import Iterable
 
 FORCE_POSITION_FACTOR = 25.
 FORCE_ROTATION_FACTOR = 5.
@@ -100,6 +101,7 @@ class EndEffectorImpedanceController(Controller):
             eef_name,
             joint_indexes,
         )
+
         # Determine whether this is pos ori or just pos
         self.use_ori = control_ori
 
@@ -112,8 +114,8 @@ class EndEffectorImpedanceController(Controller):
         # input and output max and min
         self.input_max = input_max
         self.input_min = input_min
-        self.output_max = output_max[:self.control_dim] if type(output_max) == tuple else output_max
-        self.output_min = output_min[:self.control_dim] if type(output_min) == tuple else output_min
+        self.output_max = output_max[:self.control_dim] if isinstance(output_max, Iterable) else output_max
+        self.output_min = output_min[:self.control_dim] if isinstance(output_min, Iterable) else output_min
 
         # force control
         self.force_control = force_control
