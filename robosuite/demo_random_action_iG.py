97eb148e1ecf94ac71114f9a7a7a8020f909a33b
Roberto Martin-Martin <rmartinmar@gmail.com>
2020-06-11 00:48:39 -0700

    test for iG

---
+++ robosuite/demo_random_action_iG.py
@@ -0,0 +1,66 @@
+import numpy as np
+from robosuite.controllers import load_controller_config
+from robosuite.utils.input_utils import *
+
+
+if __name__ == "__main__":
+
+    # Create dict to hold options that will be passed to env creation call
+    options = {}
+
+    # print welcome info
+    print("Welcome to Surreal Robotics Suite v{}!".format(suite.__version__))
+    print(suite.__logo__)
+
+    # Choose environment and add it to options
+    options["env_name"] = choose_environment()
+
+    # If a multi-arm environment has been chosen, choose configuration and appropriate robot(s)
+    if "TwoArm" in options["env_name"]:
+        # Choose env config and add it to options
+        options["env_configuration"] = choose_multi_arm_config()
+
+        # If chosen configuration was bimanual, the corresponding robot must be Baxter. Else, have user choose robots
+        if options["env_configuration"] == 'bimanual':
+            options["robots"] = 'Baxter'
+        else:
+            options["robots"] = []
+
+            # Have user choose two robots
+            print("A multiple single-arm configuration was chosen.\n")
+
+            for i in range(2):
+                print("Please choose Robot {}...\n".format(i))
+                options["robots"].append(choose_robots(exclude_bimanual=True))
+
+    # Else, we simply choose a single (single-armed) robot to instantiate in the environment
+    else:
+        options["robots"] = choose_robots(exclude_bimanual=True)
+
+    # Choose controller
+    controller_name = choose_controller()
+
+    # Load the desired controller
+    options["controller_configs"] = load_controller_config(default_controller=controller_name)
+
+    # Help message to user
+    print()
+    env = suite.make(
+        **options,
+        has_renderer=True,
+        ignore_done=True,
+        use_camera_obs=False,
+        render_with_igibson=True,
+        control_freq=20,
+        )
+    env.reset()
+    #env.viewer.set_camera(camera_id=0)
+
+    # Get action limits
+    low, high = env.action_spec
+
+    # do visualization
+    for i in range(10000):
+        action = np.random.uniform(low, high)
+        obs, reward, done, _ = env.step(action)
+        env.render()
