719616913339d76dc9bcac9a93fcdc071cebf4cc
josiah_wong <84cremebrule@gmail.com>
2020-06-22 15:22:11 -0700

    update arena textures

--- robosuite/models/arenas/table_arena.py
+++ robosuite/models/arenas/table_arena.py
@@ -12,6 +12,7 @@ class TableArena(Arena):
         table_full_size=(0.8, 0.8, 0.8),
         table_friction=(1, 0.005, 0.0001),
         table_offset=(0, 0, 0),
+        has_legs=True,
         xml="arenas/table_arena.xml",
     ):
         """
@@ -20,6 +21,8 @@ class TableArena(Arena):
             table_friction: friction parameters of the table
             table_offset: offset from center of arena when placing table
                 Note that the z value sets the upper limit of the table
+            has_legs: whether the table has legs or not
+            xml: xml file to load arena
         """
         super().__init__(xml_path_completion(xml))
 
@@ -34,6 +37,7 @@ class TableArena(Arena):
         self.table_visual = self.table_body.find("./geom[@name='table_visual']")
         self.table_top = self.table_body.find("./site[@name='table_top']")
 
+        self.has_legs = has_legs
         self.table_legs_visual = [
             self.table_body.find("./geom[@name='table_leg1_visual']"),
             self.table_body.find("./geom[@name='table_leg2_visual']"),
@@ -57,25 +61,31 @@ class TableArena(Arena):
             "pos", array_to_string(np.array([0, 0, self.table_half_size[2]]))
         )
 
-        # Set leg locations
-        delta_x = [0.1, -0.1, -0.1, 0.1]
-        delta_y = [0.1, 0.1, -0.1, -0.1]
-        for leg, dx, dy in zip(self.table_legs_visual, delta_x, delta_y):
-            # If x-length of table is less than a certain length, place leg in the middle between ends
-            # Otherwise we place it near the edge
-            x = 0
-            if self.table_half_size[0] > abs(dx * 2.0):
-                x += np.sign(dx) * self.table_half_size[0] - dx
-            # Repeat the same process for y
-            y = 0
-            if self.table_half_size[1] > abs(dy * 2.0):
-                y += np.sign(dy) * self.table_half_size[1] - dy
-            # Get z value
-            z = (self.table_offset[2] - self.table_half_size[2]) / 2.0
-            # Set leg position
-            leg.set("pos", array_to_string([x, y, -z]))
-            # Set leg size
-            leg.set("size", array_to_string([0.025, z]))
+        # If we're not using legs, set their size to 0
+        if not self.has_legs:
+            for leg in self.table_legs_visual:
+                leg.set("rgba", array_to_string([1, 0, 0, 0]))
+                leg.set("size", array_to_string([0.0001, 0.0001]))
+        else:
+            # Otherwise, set leg locations appropriately
+            delta_x = [0.1, -0.1, -0.1, 0.1]
+            delta_y = [0.1, 0.1, -0.1, -0.1]
+            for leg, dx, dy in zip(self.table_legs_visual, delta_x, delta_y):
+                # If x-length of table is less than a certain length, place leg in the middle between ends
+                # Otherwise we place it near the edge
+                x = 0
+                if self.table_half_size[0] > abs(dx * 2.0):
+                    x += np.sign(dx) * self.table_half_size[0] - dx
+                # Repeat the same process for y
+                y = 0
+                if self.table_half_size[1] > abs(dy * 2.0):
+                    y += np.sign(dy) * self.table_half_size[1] - dy
+                # Get z value
+                z = (self.table_offset[2] - self.table_half_size[2]) / 2.0
+                # Set leg position
+                leg.set("pos", array_to_string([x, y, -z]))
+                # Set leg size
+                leg.set("size", array_to_string([0.025, z]))
 
     @property
     def table_top_abs(self):
