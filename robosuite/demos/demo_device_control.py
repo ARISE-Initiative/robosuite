bbe28c87c74f548e5238d5cec74372c29599f684
josiah_wong <84cremebrule@gmail.com>
2020-09-27 12:56:59 -0700

    remove redundant / unused assets

--- robosuite/demos/demo_device_control.py
+++ robosuite/demos/demo_device_control.py
@@ -149,7 +149,7 @@ if __name__ == "__main__":
         **config,
         has_renderer=True,
         has_offscreen_renderer=False,
-        render_camera="agentview",
+        render_camera=None,#"agentview",
         ignore_done=True,
         use_camera_obs=False,
         gripper_visualizations=True,
