17024ce7765dc6c1a267a0c3c6d67178d73d17b2
Yifeng Zhu <yifeng.zhu@utexas.edu>
2020-10-06 12:31:40 -0500

    Change tabs indentation to spaces

--- robosuite/wrappers/visii_wrapper.py
+++ robosuite/wrappers/visii_wrapper.py
@@ -13,182 +13,179 @@ from robosuite.wrappers import Wrapper
 from robosuite.models.robots import Baxter, IIWA, Jaco, Kinova3, Panda, Sawyer, UR5e
 
 class VirtualWrapper(Wrapper):
+    """
 
-	env = None
+    Initializes the ViSII wrapper.
 
-	# stores the current state
-	obs_dict = {}
-	reward = 0
-	done = False
-	info = ""
+    Args:
+        env (MujocoEnv instance): The environment to wrap.
+        width (int, optional)
+        height (int, optional)
+        use_noise (bool, optional): Use noise or denoise
+        debug_mode (bool, optional): Use debug mode for visii
+    """
+    def __init__(self,
+                 env,
+                 width=500,
+                 height=500,
+                 use_noise=True,
+                 debug_mode=False):
+        super().__init__(env)
 
+        # Camera variables
+        self.width = width
+        self.height = height
 
-	def __init__(self, env):
+        if debug_mode:
+            visii.initialize_interactive()
+        else:
+            visii.initialize_headless()
 
-		"""
+        if not use_noise: 
+            visii.enable_denoiser()
 
-		Initializes the ViSII wrapper.
+        # Creates an entity
+        self.camera = visii.entity.create(
+            name = "camera",
+            transform = visii.transform.create("camera_transform"),
+        )
 
-		Args:
-			env (MujocoEnv instance): The environment to wrap.
+        # Adds a camera to the entity
+        self.camera.set_camera(
+            visii.camera.create_perspective_from_fov(
+                name = "camera_camera", 
+                field_of_view = 1, 
+                aspect = float(self.width)/float(self.height)
+            )
+        )
 
-		"""
+        # Sets the primary camera to the renderer to the camera entity
+        visii.set_camera_entity(self.camera)
 
-		self.env = env
+        # Lets place our camera to look at the scene
+        # all the position are defined by visii.vector3  
 
-		# Camera variables
-		self.width  = 500
-		self.height = 500
+        self.camera.get_transform().set_position(visii.vec3(0, 0, 1))
 
-		self.denoiser = True
+        self.camera.get_transform().look_at(
+            at = visii.vec3(-50,0,80) , # look at (world coordinate)
+            up = visii.vec3(0,0,1), # up vector
+            eye = visii.vec3(-500,500,100 + 80),
+            previous = False
+        )
 
-		visii.initialize_headless()
+        self.camera.get_camera().set_aperture_diameter(2000)
+        self.camera.get_camera().set_focal_distance(500)
 
-		if self.denoiser is True: 
-			visii.enable_denoiser()
+        self.robots = []
+        self.robots_names = self.env.robot_names
 
-		# Creates an entity
-		self.camera = visii.entity.create(
-			name = "camera",
-			transform = visii.transform.create("camera_transform"),
-		)
+        for robot in self.robot_names:
+            self.robots.append(self.str_to_class(robot)())
 
-		# Adds a camera to the entity
-		self.camera.set_camera(
-			visii.camera.create_perspective_from_fov(
-				name = "camera_camera", 
-				field_of_view = 1, 
-				aspect = float(self.width)/float(self.height)
-			)
-		)
+    def close(self):
+        visii.deinitialize()
 
-		# Sets the primary camera to the renderer to the camera entity
-		visii.set_camera_entity(self.camera)
+    def str_to_class(self, str):
+        return getattr(sys.modules[__name__], str)
 
-		# Lets place our camera to look at the scene
-		# all the position are defined by visii.vector3  
+    def reset(self):
+        self.obs_dict = self.env.reset()
 
-		self.camera.get_transform().set_position(visii.vec3(0, 0, 1))
+    def step(self, action):
 
-		self.camera.get_transform().look_at(
-			at = visii.vec3(-50,0,80) , # look at (world coordinate)
-			up = visii.vec3(0,0,1), # up vector
-			eye = visii.vec3(-500,500,100 + 80),
-			previous = False
-		)
+        """
+        Updates the states for the wrapper given a certain action
+        """
 
-		self.camera.get_camera().set_aperture_diameter(2000)
-		self.camera.get_camera().set_focal_distance(500)
+        obs_dict, reward, done, info = self.env.step(action)
+        self.update(obs_dict, reward, done, info)
 
-		self.robots = []
-		self.robots_names = self.env.robot_names
+        return obs_dict, reward, done, info
 
-		for robot in self.robot_names:
-			self.robots.append(self.str_to_class(robot)())
+    def update(self, obs_dict, reward, done, info):
+        self.obs_dict = obs_dict
+        self.reward   = reward
+        self.done     = done
+        self.info     = info
 
-	def close(self):
-		visii.deinitialize()
+    def render(self, render_type):
+        """
+        Renders the scene
+        Arg:
+            render_type: tells the method whether to save to png or save to hdr
+        """
 
-	def str_to_class(self, str):
-		return getattr(sys.modules[__name__], str)
+        # For now I am only rendering it as a png
+        
+        floor = visii.entity.create(
+            name = "floor",
+            mesh = visii.mesh.create_plane("plane"),
+            material = visii.material.create("plane"),
+            transform = visii.transform.create("plane")
+        )
+        floor.get_transform().set_scale(visii.vec3(10000))
+        floor.get_transform().set_position(visii.vec3(0, 0, -5))
+        floor.get_material().set_base_color(visii.vec3(.0))
+        floor.get_material().set_roughness(1)
+        floor.get_material().set_specular(0)
 
-	def reset(self):
-		self.obs_dict = self.env.reset()
+        # adds the object to the scene 
+        obj = visii.import_obj(
+            "obj", # prefix name
+            '../models/assets/robots/sawyer/meshes/head.obj', #obj path
+            '../models/assets/robots/sawyer/meshes/', # mtl folder 
+            visii.vec3(0,0,0), # translation 
+            visii.vec3(1), # scale here
+            visii.angleAxis(3.14 * .5, visii.vec3(1,0,0)) #rotation here
+        )
 
-	def step(self, action):
+        #obj[0].get_transform().set_position(visii.vec3(0, 0, 1))
+        #obj[0].get_material().set_base_color(visii.vec3(0.9, 0.9, 0.9))
 
-		"""
-		Updates the states for the wrapper given a certain action
-		"""
+        obj[0].get_transform().set_position(visii.vec3(0,0,1))
+        obj[0].get_transform().set_scale(visii.vec3(0.4))
+        obj[0].get_material().set_base_color(visii.vec3(0.9, 0.9, 0.9)) 
 
-		obs_dict, reward, done, info = self.env.step(action)
-		self.update(obs_dict, reward, done, info)
-
-		return obs_dict, reward, done, info
-
-	def update(self, obs_dict, reward, done, info):
-		self.obs_dict = obs_dict
-		self.reward   = reward
-		self.done     = done
-		self.info     = info
-
-	def render(self, render_type):
-		"""
-		Renders the scene
-		Arg:
-			render_type: tells the method whether to save to png or save to hdr
-		"""
-
-		# For now I am only rendering it as a png
-		
-		floor = visii.entity.create(
-			name = "floor",
-			mesh = visii.mesh.create_plane("plane"),
-			material = visii.material.create("plane"),
-			transform = visii.transform.create("plane")
-		)
-		floor.get_transform().set_scale(visii.vec3(10000))
-		floor.get_transform().set_position(visii.vec3(0, 0, -5))
-		floor.get_material().set_base_color(visii.vec3(.0))
-		floor.get_material().set_roughness(1)
-		floor.get_material().set_specular(0)
-
-		# adds the object to the scene 
-		obj = visii.import_obj(
-			"obj", # prefix name
-			'../models/assets/robots/sawyer/meshes/head.obj', #obj path
-			'../models/assets/robots/sawyer/meshes/', # mtl folder 
-			visii.vec3(0,0,0), # translation 
-			visii.vec3(1), # scale here
-			visii.angleAxis(3.14 * .5, visii.vec3(1,0,0)) #rotation here
-		)
-
-		#obj[0].get_transform().set_position(visii.vec3(0, 0, 1))
-		#obj[0].get_material().set_base_color(visii.vec3(0.9, 0.9, 0.9))
-
-		obj[0].get_transform().set_position(visii.vec3(0,0,1))
-		obj[0].get_transform().set_scale(visii.vec3(0.4))
-		obj[0].get_material().set_base_color(visii.vec3(0.9, 0.9, 0.9)) 
-
-		visii.render_to_png(
-			width = self.width,
-			height = self.height, 
-			samples_per_pixel = 500,   
-			image_path = 'temp.png'
-		)
-  
-	#def parse_mjcf_files(self):
+        visii.render_to_png(
+            width = self.width,
+            height = self.height, 
+            samples_per_pixel = 500,   
+            image_path = 'temp.png'
+        )
+        
+    #def parse_mjcf_files(self):
 
 
-	def printState(self): # For testing purposes
-		print(self.obs_dict)
+    def printState(self): # For testing purposes
+        print(self.obs_dict)
 
 if __name__ == '__main__':
 
-	env = VirtualWrapper(
-		env = suite.make(
-				"Lift",
-				robots = "Sawyer",
-				reward_shaping=True,
-				has_renderer=False,           # no on-screen renderer
-				has_offscreen_renderer=False, # no off-screen renderer
-				ignore_done=True,
-				use_object_obs=True,          # use object-centric feature
-				use_camera_obs=False,         # no camera observations
-				control_freq=10,
-			)
-	)
+    env = VirtualWrapper(
+        env = suite.make(
+                "Lift",
+                robots = "Sawyer",
+                reward_shaping=True,
+                has_renderer=False,       # no on-screen renderer
+                has_offscreen_renderer=False, # no off-screen renderer
+                ignore_done=True,
+                use_object_obs=True,      # use object-centric feature
+                use_camera_obs=False,     # no camera observations
+                control_freq=10,
+            )
+    )
 
-	env.reset()
+    env.reset()
 
-	action = np.random.randn(8)
-	obs, reward, done, info = env.step(action)
+    action = np.random.randn(8)
+    obs, reward, done, info = env.step(action)
 
-	#env.printState() # For testing purposes
+    #env.printState() # For testing purposes
 
-	env.render(render_type = "png")
+    env.render(render_type = "png")
 
-	env.close()
+    env.close()
 
-	print('Done.')
+    print('Done.')
 
