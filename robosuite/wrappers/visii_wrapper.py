bf441dde665e44f34fdc06bd9ebe07cc0c1a3fc4
Yifeng Zhu <yifeng.zhu@utexas.edu>
2020-10-06 13:09:51 -0500

    WIP: visualization of a robot part and several TODOs.

--- robosuite/wrappers/visii_wrapper.py
+++ robosuite/wrappers/visii_wrapper.py
@@ -11,6 +11,7 @@ import visii
 import robosuite as suite
 from robosuite.wrappers import Wrapper
 from robosuite.models.robots import Baxter, IIWA, Jaco, Kinova3, Panda, Sawyer, UR5e
+import open3d as o3d
 
 class VirtualWrapper(Wrapper):
     """
@@ -65,18 +66,30 @@ class VirtualWrapper(Wrapper):
         # Lets place our camera to look at the scene
         # all the position are defined by visii.vector3  
 
+        # TODO (yifeng): 1. Put camera intiialization to a seprate
+        # method
+        # TODO (yifeng): 2. Create another function to configure the camera
+        # parameters when needed
+
         self.camera.get_transform().set_position(visii.vec3(0, 0, 1))
 
         self.camera.get_transform().look_at(
-            at = visii.vec3(-50,0,80) , # look at (world coordinate)
+            at = visii.vec3(0,0,0) , # look at (world coordinate)
             up = visii.vec3(0,0,1), # up vector
-            eye = visii.vec3(-500,500,100 + 80),
+            eye = visii.vec3(1.5, 1.5, 1.5),
             previous = False
         )
 
-        self.camera.get_camera().set_aperture_diameter(2000)
-        self.camera.get_camera().set_focal_distance(500)
+        # (Yifeng): These two lines are breaking things
+        # self.camera.get_camera().set_aperture_diameter(2000)
+        # self.camera.get_camera().set_focal_distance(500)
 
+        # Environment configuration
+        self._dome_light_intensity = 1
+        visii.set_dome_light_intensity(self._dome_light_intensity)
+
+        visii.set_max_bounce_depth(2)
+        
         self.robots = []
         self.robots_names = self.env.robot_names
 
@@ -117,6 +130,23 @@ class VirtualWrapper(Wrapper):
         """
 
         # For now I am only rendering it as a png
+
+        # TODO (yifeng): Put static objects intitialization in the
+        # beginning.
+
+        light_1 = visii.entity.create(
+            name="light_1",
+            mesh=visii.mesh.create_plane("light_1"),
+            transform=visii.transform.create("light_1"),
+        )
+
+        light_1.set_light(
+            visii.light.create("light_1")
+        )
+
+        light_1.get_light().set_intensity(20000)
+        light_1.get_transform().set_scale(visii.vec3(0.3))
+        light_1.get_transform().set_position(visii.vec3(-3, -3, 2))
         
         floor = visii.entity.create(
             name = "floor",
@@ -124,28 +154,52 @@ class VirtualWrapper(Wrapper):
             material = visii.material.create("plane"),
             transform = visii.transform.create("plane")
         )
-        floor.get_transform().set_scale(visii.vec3(10000))
+        floor.get_transform().set_scale(visii.vec3(10))
         floor.get_transform().set_position(visii.vec3(0, 0, -5))
-        floor.get_material().set_base_color(visii.vec3(.0))
-        floor.get_material().set_roughness(1)
+        floor.get_material().set_base_color(visii.vec3(0.8, 1, 0.8))
+        floor.get_material().set_roughness(0.4)
         floor.get_material().set_specular(0)
 
-        # adds the object to the scene 
-        obj = visii.import_obj(
-            "obj", # prefix name
-            '../models/assets/robots/sawyer/meshes/head.obj', #obj path
-            '../models/assets/robots/sawyer/meshes/', # mtl folder 
-            visii.vec3(0,0,0), # translation 
-            visii.vec3(1), # scale here
-            visii.angleAxis(3.14 * .5, visii.vec3(1,0,0)) #rotation here
+        # adds the object to the scene
+
+        # We decide if we use obj files or stl files based on the
+        # robot description file
+        
+        # obj file extension
+        # obj = visii.import_obj(
+        #     "obj", # prefix name
+        #     '../models/assets/robots/sawyer/meshes/head.obj', #obj path
+        #     '../models/assets/robots/sawyer/meshes/', # mtl folder 
+        #     visii.vec3(0,0,0), # translation 
+        #     visii.vec3(1), # scale here
+        #     visii.angleAxis(3.14 * .5, visii.vec3(1,0,0)) #rotation here
+        # )
+        # obj[0].get_transform().set_position(visii.vec3(0,0,1))
+        # obj[0].get_transform().set_scale(visii.vec3(1.0))
+        # obj[0].get_material().set_base_color(visii.vec3(0.9, 0.9, 0.9)) 
+
+        # stl file extension
+        mesh = o3d.io.read_triangle_mesh('../models/assets/robots/sawyer/meshes/head.stl')
+        link_name = 'head'
+
+        normals = np.array(mesh.vertex_normals).flatten().tolist()
+        vertices = np.array(mesh.vertices).flatten().tolist() 
+
+        mesh = visii.mesh.create_from_data(f'{link_name}_mesh', positions=vertices, normals=normals)
+
+        link_entity = visii.entity.create(
+            name=link_name,
+            mesh=mesh,
+            transform=visii.transform.create(link_name),
+            material=visii.material.create(link_name)
         )
 
-        #obj[0].get_transform().set_position(visii.vec3(0, 0, 1))
-        #obj[0].get_material().set_base_color(visii.vec3(0.9, 0.9, 0.9))
+        link_entity.get_transform().set_position(visii.vec3(0, 0, 0.2))
 
-        obj[0].get_transform().set_position(visii.vec3(0,0,1))
-        obj[0].get_transform().set_scale(visii.vec3(0.4))
-        obj[0].get_material().set_base_color(visii.vec3(0.9, 0.9, 0.9)) 
+        link_entity.get_material().set_base_color(visii.vec3(0.2, 0.2, 0.2))
+        link_entity.get_material().set_metallic(0)
+        link_entity.get_material().set_transmission(0)
+        link_entity.get_material().set_roughness(0.3)
 
         visii.render_to_png(
             width = self.width,
@@ -160,6 +214,31 @@ class VirtualWrapper(Wrapper):
     def printState(self): # For testing purposes
         print(self.obs_dict)
 
+    def get_camera_intrinsics(self):
+        """Get camera intrinsics matrix
+
+        """
+        raise NotImplementedError
+
+    def get_camera_extrinsics(self):
+        """Get camera extrinsics matrix
+
+        """
+        raise NotImplementedError
+
+    def set_camera_intrinsics(self):
+        """Get camera extrinsics matrix
+
+        """
+        raise NotImplementedError
+
+    def set_camera_extrinsics(self):
+        """Get camera extrinsics matrix
+
+        """
+        raise NotImplementedError
+        
+        
 if __name__ == '__main__':
 
     env = VirtualWrapper(
@@ -173,7 +252,8 @@ if __name__ == '__main__':
                 use_object_obs=True,      # use object-centric feature
                 use_camera_obs=False,     # no camera observations
                 control_freq=10,
-            )
+            ),
+        use_noise=False,
     )
 
     env.reset()
