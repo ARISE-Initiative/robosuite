7fb4c09a3a6d0b1e393338872c6e4006830584cf
amandlek <amandlek@users.noreply.github.com>
2021-02-09 17:21:05 -0800

    Update __init__.py

--- robosuite/devices/__init__.py
+++ robosuite/devices/__init__.py
@@ -5,4 +5,4 @@ try:
 except ImportError:
     print("""Unable to load module hid, required to interface with SpaceMouse.\n
            Only Mac OS X is officially supported. Install the additional\n
-           requirements with `pip install -r requirements-ik.txt`""")
+           requirements with `pip install -r requirements-extra.txt`""")
