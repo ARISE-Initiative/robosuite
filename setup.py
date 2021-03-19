a5d85d9e6ac275a650206513b9bfbbaa87d95647
Yuke Zhu <zyk0902@gmail.com>
2021-03-18 19:37:24 -0500

    update mujoco-py version in setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
         "numpy>=1.13.3",
         "numba>=0.49.1",
         "scipy>=1.2.3",
-        "mujoco-py==2.0.2.9",
+        "mujoco-py==2.0.2.13",
     ],
     eager_resources=['*'],
     include_package_data=True,
