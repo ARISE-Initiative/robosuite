59aaf56810a955d82dd72a724041998eeb16bd26
Soroush Nasiriany <snasiriany@users.noreply.github.com>
2022-02-03 15:28:15 -0600

    Update version number to v1.3.1
    Due to texture changes from Abhishek's PR

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     author="Yuke Zhu, Josiah Wong, Ajay Mandlekar, Roberto Martín-Martín",
     url="https://github.com/ARISE-Initiative/robosuite",
     author_email="yukez@cs.utexas.edu",
-    version="1.3.0",
+    version="1.3.1",
     long_description=long_description,
     long_description_content_type="text/markdown",
 )
