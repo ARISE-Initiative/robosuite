a95c9af174a447785c6af6f558d7bd18e6c6ea83
Yuke Zhu <yukez@cs.utexas.edu>
2022-03-13 15:59:11 -0500

    Update version number to 1.3.2

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     author="Yuke Zhu, Josiah Wong, Ajay Mandlekar, Roberto Martín-Martín",
     url="https://github.com/ARISE-Initiative/robosuite",
     author_email="yukez@cs.utexas.edu",
-    version="1.3.1",
+    version="1.3.2",
     long_description=long_description,
     long_description_content_type="text/markdown",
 )
