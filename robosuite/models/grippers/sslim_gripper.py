950e28d6453dd1ffa5001fe7b0534523b8b3a4d6
Michael Lin <michaelv03@gmail.com>
2022-06-09 11:25:28 -0700

    adding sslim gripper temporary files

---
+++ robosuite/models/grippers/sslim_gripper.py
@@ -0,0 +1,35 @@
+"""
+SSLIM Gripper
+"""
+import numpy as np
+
+from robosuite.models.grippers.gripper_model import GripperModel
+from robosuite.utils.mjcf_utils import xml_path_completion
+
+
+class SSLIMGripper(GripperModel):
+    """
+    Revolute manipulator.
+
+    Args:
+        idn (int or str): Number or some other unique identification string for this gripper instance
+    """
+
+    def __init__(self, idn=0):
+        super().__init__(xml_path_completion("grippers/sslim.xml"), idn=idn)
+
+    def format_action(self, action):
+        return action
+
+    @property
+    def init_qpos(self):
+        return np.zeros(9)
+
+    @property
+    def _important_geoms(self):
+        return {
+            "left_finger": [],
+            "right_finger": [],
+            "left_fingerpad": [],
+            "right_fingerpad": [],
+        }
