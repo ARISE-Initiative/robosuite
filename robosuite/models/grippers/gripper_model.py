validate all tests work, fix eef ik rotation offset bug, update infra accordingly

robosuite/models/grippers/gripper_model.py
@@ -4,7 +4,8 @@ Defines the base class of all grippers
 import numpy as np
 
 from robosuite.models.base import MujocoXMLModel
-from robosuite.utils.mjcf_utils import GRIPPER_COLLISION_COLOR
+from robosuite.utils.mjcf_utils import GRIPPER_COLLISION_COLOR, find_elements, string_to_array
+import robosuite.utils.transform_utils as T
 
 
 class GripperModel(MujocoXMLModel):
@@ -23,9 +24,16 @@ class GripperModel(MujocoXMLModel):
         self.current_action = np.zeros(self.dof)
 
         # Grab gripper offset (string -> np.array -> elements [1, 2, 3, 0] (x, y, z, w))
-        self.rotation_offset = np.fromstring(
+        # This is the comopunded rotation with the base body and the eef body as well!
+        base_quat = np.fromstring(
             self.worldbody[0].attrib.get("quat", "1 0 0 0"), dtype=np.float64, sep=" "
         )[[1, 2, 3, 0]]
+        eef_element = find_elements(
+            root=self.root, tags="body", attribs={"name": self.correct_naming("eef")}, return_first=True
+        )
+        eef_relative_quat = string_to_array(eef_element.get("quat", "1 0 0 0"))[[1, 2, 3, 0]]
+        self.rotation_offset = T.quat_multiply(eef_relative_quat, base_quat)
+
 
     def format_action(self, action):
         """

robosuite/utils/mjcf_utils.py
@@ -275,7 +275,7 @@ def string_to_array(string):
     Returns:
         np.array: Numerical array equivalent of @string
     """
-    return np.array([float(x) for x in string.split(" ")])
+    return np.array([float(x) for x in string.strip().split(" ")])
 
 
 def convert_to_string(inp):

tests/test_controllers/test_linear_interpolator.py
@@ -140,10 +140,10 @@ def test_linear_interpolator():
 
                 # Keep track of state of robot eef (pos, ori (euler)) and torques
                 current_torques = np.zeros(7)
-                initial_state = [env.robots[0]._hand_pos, T.mat2euler(env.robots[0]._hand_orn)]
+                initial_state = [env.robots[0]._hand_pos, T.mat2quat(env.robots[0]._hand_orn)]
                 dstate = [
                     env.robots[0]._hand_pos - initial_state[0],
-                    T.mat2euler(env.robots[0]._hand_orn) - initial_state[1],
+                    T.mat2euler(T.quat2mat(T.quat_distance(T.mat2quat(env.robots[0]._hand_orn), initial_state[1]))),
                 ]
 
                 # Define the uniform trajectory action
@@ -172,12 +172,12 @@ def test_linear_interpolator():
                     timesteps[j] += 1
                     dstate = [
                         env.robots[0]._hand_pos - initial_state[0],
-                        T.mat2euler(env.robots[0]._hand_orn) - initial_state[1],
+                        T.mat2euler(T.quat2mat(T.quat_distance(T.mat2quat(env.robots[0]._hand_orn), initial_state[1]))),
                     ]
 
                 # When finished, print out the timestep results
                 print(
-                    "Completed trajectory. Total summed absolute delta torques: {}".format(summed_abs_delta_torques[j])
+                    "Completed trajectory. Avg per-step absolute delta torques: {}".format(summed_abs_delta_torques[j] / timesteps[j])
                 )
 
                 # Shut down this env before starting the next test