fe6280eabf0d995f02144499d42ca2cc6627c9d0
Abhishek Joshi <abhijoshi@Abhisheks-MBP.attlocal.net>
2022-11-25 09:07:29 -0600

    adding script changes for new mujoco bindings

--- robosuite/scripts/browse_mjcf_model.py
+++ robosuite/scripts/browse_mjcf_model.py
@@ -9,9 +9,12 @@ Example:
 import argparse
 import os
 
-from mujoco_py import MjSim, MjViewer, load_model_from_path
+# from mujoco import MjSim, MjViewer, load_model_from_path
+import mujoco
 
 import robosuite as suite
+from robosuite.utils import OpenCVRenderer
+from robosuite.utils.binding_utils import MjRenderContext, MjSim
 
 if __name__ == "__main__":
 
@@ -21,9 +24,11 @@ if __name__ == "__main__":
     parser.add_argument("--filepath", type=str, default=arena_file)
     args = parser.parse_args()
 
-    model = load_model_from_path(args.filepath)
+    model = mujoco.MjModel.from_xml_path(args.filepath)
     sim = MjSim(model)
-    viewer = MjViewer(sim)
+    render_context = MjRenderContext(sim)
+    sim.add_render_context(render_context)
+    viewer = OpenCVRenderer(sim)
 
     print("Press ESC to exit...")
     while True:
