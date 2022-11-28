a0a47ec92bc72413cb9f83a6f5c9ba2a41d796ac
Yifeng Zhu <yifeng.zhu@utexas.edu>
2022-11-27 20:10:04 -0600

    Add comments to log utils

--- robosuite/utils/log_utils.py
+++ robosuite/utils/log_utils.py
@@ -22,18 +22,21 @@ MESSAGE_STR = "%(message)s (%(filename)s:%(lineno)d)"
 
 
 class FileFormatter(logging.Formatter):
+    """Formatter class of logging for file logging."""
     FORMATS = {
         levelno: colored(FORMAT_STR["file"], color, attrs=["bold"]) + MESSAGE_STR
         for (levelno, color) in LEVEL_COLORS.items()
     }
 
     def format(self, record):
+        """Apply custom fomatting on LogRecord object record."""
         log_fmt = self.FORMATS.get(record.levelno)
         formatter = logging.Formatter(log_fmt, "%Y-%m-%d %H:%M:%S")
         return formatter.format(record)
 
 
 class ConsoleFormatter(logging.Formatter):
+    """Formatter class of logging for console logging."""
     FORMATS = {
         logging.DEBUG: FORMAT_STR["console"] + MESSAGE_STR,
         logging.INFO: "%(message)s",
@@ -43,13 +46,21 @@ class ConsoleFormatter(logging.Formatter):
     }
 
     def format(self, record):
+        """Apply custom fomatting on LogRecord object record."""
         log_fmt = self.FORMATS.get(record.levelno)
         formatter = logging.Formatter(log_fmt)
         return formatter.format(record)
 
 
 class DefaultLogger:
+    """Default logger class in robosuite codebase."""
     def __init__(self, logger_name="robosuite_logs", console_logging_level="INFO", file_logging_level=None):
+        """
+        Args:
+            logger_name (str, optional): logger name. Defaults to "robosuite_logs".
+            console_logging_level (str, optional): logging level for console logging. Defaults to "INFO".
+            file_logging_level (_type_, optional): logging level for file logging. Defaults to None.
+        """
         self.logger_name = logger_name
         logger = logging.getLogger(self.logger_name)
 
@@ -68,6 +79,11 @@ class DefaultLogger:
             logger.addHandler(ch)
 
     def get_logger(self):
+        """_summary_
+
+        Returns:
+            DefaultLogger: The retrieved logger whose name equals self.logger_name
+        """
         logger = logging.getLogger(self.logger_name)
         return logger
 
