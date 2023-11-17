dbd77838e56807c1dcb223e960f6479a96a70d5f
Cristian Beltran <cristianbehe@gmail.com>
2023-11-17 10:54:22 +0900

    Update robotiq 140 gripper model

--- tests/test_grippers/test_robotiq_140.py
+++ tests/test_grippers/test_robotiq_140.py
@@ -11,12 +11,13 @@ def robotiq_tester(render, total_iters=1, test_y=True):
         gripper=gripper,
         pos="0 0 0.3",
         quat="0 0 1 0",
-        gripper_low_pos=0.02,
+        gripper_low_pos=0.03,
         gripper_high_pos=0.1,
-        box_size=[0.025] * 3,
+        box_size=[0.015] * 3,
         render=render,
     )
     tester.start_simulation()
+    tester.viewer.set_camera(0)
     tester.loop(total_iters=total_iters, test_y=test_y)
     tester.close()
 
