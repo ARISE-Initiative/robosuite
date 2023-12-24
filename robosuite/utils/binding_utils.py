4ba318ea61e19161c9813e11b889410d04ad1040
daebangstn <gunho1107@naver.com>
2023-12-25 00:11:06 +0900

    default MUJOCO_GL for Windows missed

--- robosuite/utils/binding_utils.py
+++ robosuite/utils/binding_utils.py
@@ -39,7 +39,9 @@ if macros.MUJOCO_GPU_RENDERING and os.environ.get("MUJOCO_GL", None) not in ["os
     # option for rendering
     if _SYSTEM == "Darwin":
         os.environ["MUJOCO_GL"] = "cgl"
-    else:
+    elif _SYSTEM == "Windows":
+        os.environ["MUJOCO_GL"] = "wgl"
+    elif _SYSTEM == "Linux":
         os.environ["MUJOCO_GL"] = "egl"
 _MUJOCO_GL = os.environ.get("MUJOCO_GL", "").lower().strip()
 if _MUJOCO_GL not in ("disable", "disabled", "off", "false", "0"):
