69a88ce1b31ad83ba8bd33fad1e3be1be6b21e28
vivekbagade <vivek.bagade92@gmail.com>
2024-06-05 22:07:59 -0700

    Add episode replay and episode_len padding

--- robosuite/recorder/recorder.py
+++ robosuite/recorder/recorder.py
@@ -6,6 +6,7 @@ import os
 
 cam_height = 256
 cam_width = 256
+episode_len = 800
 
 class Recorder:
     def __init__(self, cameras, task) -> None:
@@ -35,6 +36,18 @@ class Recorder:
         if max_timesteps < 10:
             print('Not enough steps to save episode')
             return
+        if max_timesteps > episode_len:
+            print('recording longer than expected, skipping save')
+            return
+
+        # padding to episode_len        
+        pad_len = episode_len - max_timesteps
+        self.data_dict['/observations/qpos'] = np.pad(self.data_dict['/observations/qpos'], ((0, pad_len), (0, 0)), mode='constant')
+        self.data_dict['/observations/qvel'] = np.pad(self.data_dict['/observations/qvel'], ((0, pad_len), (0, 0)), mode='constant')
+        self.data_dict['/action'] = np.pad(self.data_dict['/action'], ((0, pad_len), (0, 0)), mode='constant')
+        for cam_name in self.cameras:
+            self.data_dict[f'/observations/images/{cam_name}'] = np.pad(self.data_dict[f'/observations/images/{cam_name}'], ((0, pad_len), (0, 0), (0, 0), (0, 0)), mode='constant')
+
         # create data dir if it doesn't exist
         self.data_dir = os.path.join(self.data_dir, self.task)
         if not os.path.exists(self.data_dir): os.makedirs(self.data_dir)
@@ -48,12 +61,12 @@ class Recorder:
             obs = root.create_group('observations')
             image = obs.create_group('images')
             for cam_name in self.cameras:
-                _ = image.create_dataset(cam_name, (max_timesteps, cam_height, cam_width, 3), dtype='uint8',
+                _ = image.create_dataset(cam_name, (episode_len, cam_height, cam_width, 3), dtype='uint8',
                                         chunks=(1, cam_height, cam_width, 3), )
-            qpos = obs.create_dataset('qpos', (max_timesteps, 8))
-            qvel = obs.create_dataset('qvel', (max_timesteps, 8))
-            # image = obs.create_dataset("image", (max_timesteps, 240, 320, 3), dtype='uint8', chunks=(1, 240, 320, 3))
-            action = root.create_dataset('action', (max_timesteps, 8))
+            qpos = obs.create_dataset('qpos', (episode_len, 8))
+            qvel = obs.create_dataset('qvel', (episode_len, 8))
+            # image = obs.create_dataset("image", (episode_len, 240, 320, 3), dtype='uint8', chunks=(1, 240, 320, 3))
+            action = root.create_dataset('action', (episode_len, 8))
             
             for name, array in self.data_dict.items():
                 root[name][...] = array
