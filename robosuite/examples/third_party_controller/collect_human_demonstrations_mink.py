cc40df06144c45a4662b84373588d4be549bd667
snasiriany <snasiriany@gmail.com>
2024-09-26 12:00:32 -0500

    remove misc collect_demos scripts

--- robosuite/examples/third_party_controller/collect_human_demonstrations_mink.py
+++ robosuite/examples/third_party_controller/collect_human_demonstrations_mink.py
@@ -21,6 +21,7 @@ import robosuite.examples.third_party_controller.mink_controller
 import robosuite.macros as macros
 from robosuite.controllers import load_composite_controller_config
 from robosuite.examples.third_party_controller.mink_controller import WholeBodyMinkIKCompositeController
+from robosuite.scripts.collect_human_demonstrations import gather_demonstrations_as_hdf5, get_target, set_target
 from robosuite.utils import transform_utils
 from robosuite.utils.input_utils import input2action
 from robosuite.wrappers import DataCollectionWrapper, VisualizationWrapper
@@ -33,27 +34,6 @@ python robosuite/examples/third_party_controller/collect_human_demonstrations_mo
 Need to Esc; Tab; Scroll to Group Enable, then press Group 2. Then, double click on the mocap cube and Ctrl+left or right click.
 
 """
-
-
-def set_target(sim, target_pos=None, target_mat=None, mocap_name: str = "target"):
-    mocap_id = sim.model.body(mocap_name).mocapid[0]
-    if target_pos is not None:
-        sim.data.mocap_pos[mocap_id] = target_pos
-    if target_mat is not None:
-        # convert mat to quat
-        target_quat = np.empty(4)
-        mujoco.mju_mat2Quat(target_quat, target_mat.reshape(9, 1))
-        sim.data.mocap_quat[mocap_id] = target_quat
-
-
-def get_target(sim, mocap_name: str = "target"):
-    mocap_id = sim.model.body(mocap_name).mocapid[0]
-    target_pos = np.copy(sim.data.mocap_pos[mocap_id])
-    target_quat = np.copy(sim.data.mocap_quat[mocap_id])
-    target_mat = np.empty(9)
-    mujoco.mju_quat2Mat(target_mat, target_quat)
-    target_mat = target_mat.reshape(3, 3)
-    return target_pos, target_mat
 
 
 def collect_human_trajectory(env, device, arm, env_configuration, end_effector: str = "right", use_mocap: bool = False):
@@ -207,97 +187,6 @@ def collect_human_trajectory(env, device, arm, env_configuration, end_effector: 
 
     # cleanup for end of data collection episodes
     env.close()
-
-
-def gather_demonstrations_as_hdf5(directory, out_dir, env_info):
-    """
-    Gathers the demonstrations saved in @directory into a
-    single hdf5 file.
-
-    The strucure of the hdf5 file is as follows.
-
-    data (group)
-        date (attribute) - date of collection
-        time (attribute) - time of collection
-        repository_version (attribute) - repository version used during collection
-        env (attribute) - environment name on which demos were collected
-
-        demo1 (group) - every demonstration has a group
-            model_file (attribute) - model xml string for demonstration
-            states (dataset) - flattened mujoco states
-            actions (dataset) - actions applied during demonstration
-
-        demo2 (group)
-        ...
-
-    Args:
-        directory (str): Path to the directory containing raw demonstrations.
-        out_dir (str): Path to where to store the hdf5 file.
-        env_info (str): JSON-encoded string containing environment information,
-            including controller and robot info
-    """
-
-    hdf5_path = os.path.join(out_dir, "demo.hdf5")
-    f = h5py.File(hdf5_path, "w")
-
-    # store some metadata in the attributes of one group
-    grp = f.create_group("data")
-
-    num_eps = 0
-    env_name = None  # will get populated at some point
-
-    for ep_directory in os.listdir(directory):
-
-        state_paths = os.path.join(directory, ep_directory, "state_*.npz")
-        states = []
-        actions = []
-        success = False
-
-        for state_file in sorted(glob(state_paths)):
-            dic = np.load(state_file, allow_pickle=True)
-            env_name = str(dic["env"])
-
-            states.extend(dic["states"])
-            for ai in dic["action_infos"]:
-                actions.append(ai["actions"])
-            success = success or dic["successful"]
-
-        if len(states) == 0:
-            continue
-
-        # Add only the successful demonstration to dataset
-        if success:
-            print("Demonstration is successful and has been saved")
-            # Delete the last state. This is because when the DataCollector wrapper
-            # recorded the states and actions, the states were recorded AFTER playing that action,
-            # so we end up with an extra state at the end.
-            del states[-1]
-            assert len(states) == len(actions)
-
-            num_eps += 1
-            ep_data_grp = grp.create_group("demo_{}".format(num_eps))
-
-            # store model xml as an attribute
-            xml_path = os.path.join(directory, ep_directory, "model.xml")
-            with open(xml_path, "r") as f:
-                xml_str = f.read()
-            ep_data_grp.attrs["model_file"] = xml_str
-
-            # write datasets for states and actions
-            ep_data_grp.create_dataset("states", data=np.array(states))
-            ep_data_grp.create_dataset("actions", data=np.array(actions))
-        else:
-            print("Demonstration is unsuccessful and has NOT been saved")
-
-    # write dataset attributes (metadata)
-    now = datetime.datetime.now()
-    grp.attrs["date"] = "{}-{}-{}".format(now.month, now.day, now.year)
-    grp.attrs["time"] = "{}:{}:{}".format(now.hour, now.minute, now.second)
-    grp.attrs["repository_version"] = suite.__version__
-    grp.attrs["env"] = env_name
-    grp.attrs["env_info"] = env_info
-
-    f.close()
 
 
 if __name__ == "__main__":
