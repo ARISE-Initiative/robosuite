1cf34817d3b37bedb4789030a6639bdba5e2828d
snasiriany <snasiriany@gmail.com>
2024-09-26 15:17:57 -0500

    simplify interface for loading composite controller: specify either controller type or json file

--- robosuite/controllers/composite/composite_controller_factory.py
+++ robosuite/controllers/composite/composite_controller_factory.py
@@ -16,19 +16,14 @@ def validate_composite_controller_config(config: dict):
             raise ValueError
 
 
-def load_composite_controller_config(
-    custom_fpath: str = None, default_controller: str = None, robot: str = None
-) -> Optional[Dict]:
+def load_composite_controller_config(controller: str = None, robot: str = None) -> Optional[Dict]:
     """
     Utility function that loads the desired composite controller and returns the loaded configuration as a dict
 
-    If @default_controller is specified, any value inputted to @custom_fpath is overridden and the default controller
-    configuration is automatically loaded. See specific arg description below for available default controllers.
+    TODO
 
     Args:
-        custom_fpath: Absolute filepath to the custom controller configuration .json file to be loaded
-        default_controller: If specified, overrides @custom_fpath and loads a default configuration file for the
-            specified controller.
+        controller: TODO.
         robot: Name of the robot to load the controller for. Currently only supports "GR1"
 
     Returns:
@@ -39,16 +34,22 @@ def load_composite_controller_config(
         AssertionError: [No controller specified]
     """
     composite_controller_config = None
-    # First check if default controller is not None; if it is not, load the appropriate controller
-    if default_controller is not None:
 
+    if controller is None:
+        controller = "HYBRID_MOBILE_BASE"
+    controller_is_path = controller.endswith(".json")
+    # First check if default controller is not None; if it is not, load the appropriate controller
+    if controller_is_path:
+        controller_fpath = controller
+        ROBOSUITE_DEFAULT_LOGGER.info("Loading custom controller configuration from: {} ...".format(controller))
+    else:
         # Assert that requested default controller is in the available default controllers
         from robosuite.controllers.composite import ALL_COMPOSITE_CONTROLLERS
 
         assert (
-            default_controller in ALL_COMPOSITE_CONTROLLERS
+            controller in ALL_COMPOSITE_CONTROLLERS
         ), "Error: Unknown default controller specified. Requested {}, " "available controllers: {}".format(
-            default_controller, list(ALL_COMPOSITE_CONTROLLERS)
+            controller, list(ALL_COMPOSITE_CONTROLLERS)
         )
 
         if "GR1" in robot:
@@ -61,34 +62,30 @@ def load_composite_controller_config(
             robot_name = robot.lower()
 
         # Store the default controller config fpath associated with the requested controller
-        custom_fpath = (
+        controller_fpath = (
             pathlib.Path(robosuite.__file__).parent
-            / f"controllers/config/robots/default_{default_controller.lower()}_{robot_name}.json"
+            / f"controllers/config/robots/default_{controller.lower()}_{robot_name}.json"
         )
 
-        if not custom_fpath.exists():
-            custom_fpath = (
+        if not controller_fpath.exists():
+            controller_fpath = (
                 pathlib.Path(robosuite.__file__).parent
-                / f"controllers/config/default/composite/{default_controller.lower()}.json"
+                / f"controllers/config/default/composite/{controller.lower()}.json"
             )
             ROBOSUITE_DEFAULT_LOGGER.warn(
-                f"Default controller config for {default_controller} not found for robot {robot}. Loading default controller config for {default_controller}. The default config is defined in {custom_fpath} "
+                f"Default controller config for {controller} not found for robot {robot}. Loading default controller config for {controller}. The default config is defined in {controller_fpath} "
             )
 
-    else:
-        ROBOSUITE_DEFAULT_LOGGER.info("Loading custom controller configuration from: {} ...".format(custom_fpath))
-    #     return None
-
     # Assert that the fpath to load the controller is not empty
-    assert custom_fpath is not None, f"Error: Either custom_fpath or {default_controller} must be specified!"
+    assert controller_fpath is not None, f"Error: Either custom_fpath or {controller} must be specified!"
 
     # Attempt to load the controller
     try:
-        with open(custom_fpath) as f:
+        with open(controller_fpath) as f:
             composite_controller_config = json.load(f)
     except FileNotFoundError:
         ROBOSUITE_DEFAULT_LOGGER.error(
-            "Error opening controller filepath at: {}. " "Please check filepath and try again.".format(custom_fpath)
+            "Error opening controller filepath at: {}. " "Please check filepath and try again.".format(controller_fpath)
         )
 
     validate_composite_controller_config(composite_controller_config)
