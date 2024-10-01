836ba7116769a5d4e79d56d596e29204a76ceebc
Abhishek Joshi <abhishekjoshinj@gmail.com>
2024-09-30 21:06:11 -0400

    Removing try-except from composite controller test case

--- tests/test_controllers/test_composite_controllers.py
+++ tests/test_controllers/test_composite_controllers.py
@@ -13,50 +13,37 @@ def test_basic_controller_predefined_robots():
     (i.e., ALL_ROBOTS)
     """
 
-    success = True
-
     for robot in ALL_ROBOTS:
+        controller_config = load_composite_controller_config(
+            controller="BASIC",
+            robot=robot,
+        )
 
-        try:
-            controller_config = load_composite_controller_config(
-                controller="BASIC",
-                robot=robot,
-            )
+        config = {
+            "env_name": "Lift",
+            "robots": robot,
+            "controller_configs": controller_config,
+        }
 
-            config = {
-                "env_name": "Lift",
-                "robots": robot,
-                "controller_configs": controller_config,
-            }
+        env = suite.make(
+            **config,
+            has_renderer=False,
+            has_offscreen_renderer=False,
+            ignore_done=True,
+            use_camera_obs=False,
+            reward_shaping=True,
+            control_freq=20,
+        )
+        env.reset()
 
-            env = suite.make(
-                **config,
-                has_renderer=False,
-                has_offscreen_renderer=False,
-                ignore_done=True,
-                use_camera_obs=False,
-                reward_shaping=True,
-                control_freq=20,
-            )
-            env.reset()
+        low, high = env.action_spec
+        
+        # Runs a few steps of the simulation as a sanity check
+        for i in range(10):
+            action = np.random.uniform(low, high)
+            obs, reward, done, _ = env.step(action)
 
-            low, high = env.action_spec
-            
-            # Runs a few steps of the simulation as a sanity check
-            for i in range(10):
-                action = np.random.uniform(low, high)
-                obs, reward, done, _ = env.step(action)
-
-            env.close()
-    
-        except Exception as e:
-            success = False
-            print("----------------", file=sys.stderr)
-            print(f"An exception occurred when loading {robot} robot.", file=sys.stderr)
-            traceback.print_exc()
-            print("----------------", file=sys.stderr)
-
-    assert success
+        env.close()    
 
 if __name__ == "__main__":
     test_basic_controller_predefined_robots()
