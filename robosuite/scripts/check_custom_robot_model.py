ad952ad187371148be9180204aa9a86f6dd17901
Yifeng Zhu <yifeng.zhu@utexas.edu>
2024-10-21 00:59:30 +0800

    Update printing information

--- robosuite/scripts/check_custom_robot_model.py
+++ robosuite/scripts/check_custom_robot_model.py
@@ -1,6 +1,9 @@
 from xml.etree import ElementTree as ET
 import argparse
 
+from robosuite.robots.robot import Robot
+from robosuite.controllers.composite.composite_controller_factory import load_composite_controller_config
+
 from robosuite.utils.log_utils import ROBOSUITE_DEFAULT_LOGGER as logger
 
 def check_parts(world_body, part_name, parts_dict):
@@ -8,12 +11,22 @@ def check_parts(world_body, part_name, parts_dict):
         if part_name in joint.attrib["name"] == part_name:
             parts_dict[part_name].append(joint.attrib["name"])
 
-def check_robot_definition(xml_file):
-    root = ET.parse(xml_file).getroot()
+def check_robot_definition(robot_name):
+
+    print(f"Loading {robot_name} ...")
+    controller_config = load_composite_controller_config("BASIC")
+    robot = Robot(robot_type=robot_name,      composite_controller_config=controller_config, gripper_type=None)
+    logger.info(f"Succcessfully found the defined robot")
+
+    robot.load_model()
+    robot_model = robot.robot_model
+    root = robot_model.tree.getroot()
+    logger.info(f"Successfully loaded the xml file of robot model.")
+
     # get all joints
     world_body = root.find(".//worldbody")
     parts_dict = {}
-    for part_name in ["torso", "head", "leg"]:
+    for part_name in ["torso", "head", "leg", "gripper"]:
         parts_dict[part_name] = []
         for joint in world_body.findall(".//joint"):
             if part_name in joint.attrib["name"]:
@@ -23,7 +36,7 @@ def check_robot_definition(xml_file):
     print(len(world_body.findall(".//joint")))
     for joint in world_body.findall(".//joint"):
         is_non_arm_part = False
-        for non_arm_part_name in ["torso", "head", "leg"]:
+        for non_arm_part_name in ["torso", "head", "leg", "gripper"]:
             if non_arm_part_name in joint.attrib["name"]:
                 is_non_arm_part = True
                 break
@@ -31,30 +44,43 @@ def check_robot_definition(xml_file):
             parts_dict["arm"].append(joint.attrib["name"])
     total_counted_joints = sum([len(parts_dict[part_name]) for part_name in parts_dict.keys()])
     if total_counted_joints != len(world_body.findall(".//joint")):
-        logger.erro(f"Error in {file}")
+        logger.error(f"Error in {file}")
         logger.error(f"Counted {total_counted_joints} joints, but found {len(world_body.findall('.//joint'))} joints")
     else:
         logger.info(f"Your joint definition aligns with robosuite convention.")
         # remove empty list entries
         parts_dict = {part_name: joints for part_name, joints in parts_dict.items() if joints}
-        print(f"Robosuite will be able to detect the following body parts:  {list(parts_dict.keys())}")
-        print("For each body part, you have defined the following joints:")
+        print(f"    • Robosuite will be able to detect the following body parts:  {list(parts_dict.keys())}")
+        print("    • For each body part, you have defined the following joints:")
         for part_name in parts_dict.keys():
-            print(f"{part_name} - {len(parts_dict[part_name])} joints: {parts_dict[part_name]}")
+            print(f"        • {part_name} - {len(parts_dict[part_name])} joints: {parts_dict[part_name]}")
 
-    # checking the order of arm joints defined.
     if parts_dict.get("arm") is not None and parts_dict["arm"] != []:
+    
+        # checking the order of arm joints defined.
         arm_joint_names = parts_dict["arm"]
         first_arm_joint_name = arm_joint_names[0]
         if "l_" in first_arm_joint_name or "left" in first_arm_joint_name:
             logger.warning("Incorrect order of the arm joints. THe arm joints needs to be defined first for the right arm and then for the left arm.")
-    
+
+        # check if the body for mounting exists
+        num_arms = len(robot.arms)
+
+
+        mount_names = [robot.robot_model._eef_name[name] for name in robot.robot_model._eef_name]
+        for mount_name in mount_names[:num_arms]:
+            # find the body name of mount_name in the worldbody
+            mount_body = world_body.find(f".//body[@name='robot0_{mount_name}']")
+            if mount_body is None:
+                logger.error(f"Error: No body named '{mount_name}' found in the worldbody. Please make sure you have defined the body for mounting the arm.")
+                exit(1)
+        print("        • Grippers will be mounted on the following bodies ", mount_names[:num_arms])
+
     logger.warning("Attention!!! Make sure your definition of the motors for arms are in the same order as the joints defined. This program does not check on this item.")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--file", type=str, required=True)
-    parser.add_argument("--type", type=str, choices=["robot", "gripper"], required=True)
+    parser.add_argument("--robot", type=str, default="Panda")
     args = parser.parse_args()
-    if args.type == "robot":
-        check_robot_definition(args.file)
+    if args.robot is not None:
+        check_robot_definition(args.robot)
