afe5b2070b0f187e3be105467ba9806aac57d610
Kevin Lin <kevin.lin20000@gmail.com>
2024-10-25 01:59:00 -0500

    Fix prints, update to ALL_COMPOSITE_CONTROLLERS

--- robosuite/utils/input_utils.py
+++ robosuite/utils/input_utils.py
@@ -46,16 +46,14 @@ def choose_controller():
     Returns:
         str: Chosen controller name
     """
-    # TODO: these no longer work
     # get the list of all controllers
-    # controllers_info = suite.controllers.CONTROLLER_INFO
-    controllers = list(suite.ALL_PART_CONTROLLERS)
+    controllers = list(suite.ALL_COMPOSITE_CONTROLLERS)
 
     # Select controller to use
     print("Here is a list of controllers in the suite:\n")
 
     for k, controller in enumerate(controllers):
-        print("[{}] {} - {}".format(k, controller, None))
+        print("[{}] {}".format(k, controller))
     print()
     try:
         s = input("Choose a controller for the robot " + "(enter a number from 0 to {}): ".format(len(controllers) - 1))
