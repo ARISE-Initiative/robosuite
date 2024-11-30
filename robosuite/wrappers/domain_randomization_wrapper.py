795c4ba2667909331ff93598844bfb8e55319721
Kevin Lin <kevin.lin20000@gmail.com>
2024-11-30 12:55:58 -0600

    Add rs_assert for better formatting

--- robosuite/wrappers/domain_randomization_wrapper.py
+++ robosuite/wrappers/domain_randomization_wrapper.py
@@ -5,6 +5,7 @@ robosuite environments.
 import mujoco
 import numpy as np
 
+from robosuite.utils.log_utils import rs_assert
 from robosuite.utils.mjmod import CameraModder, DynamicsModder, LightingModder, TextureModder
 from robosuite.wrappers import Wrapper
 
@@ -155,9 +156,12 @@ class DomainRandomizationWrapper(Wrapper):
         self.modders = []
 
         if self.randomize_color:
-            assert mujoco.__version__ == "3.1.1", (
-                "TextureModder requires mujoco version 3.1.1 to run. "
-                "Pending support for later versions. Alternatively, you can set randomize_color=False."
+            rs_assert(
+                mujoco.__version__ == "3.1.1",
+                (
+                    "TextureModder requires mujoco version 3.1.1 to run. "
+                    "Pending support for later versions. Alternatively, you can set randomize_color=False."
+                ),
             )
             self.tex_modder = TextureModder(
                 sim=self.env.sim, random_state=self.random_state, **self.color_randomization_args
