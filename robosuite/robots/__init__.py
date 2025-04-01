c753fac53d4fce280ce8ab7589130926c5069acc
Edward Sun <edward.sun2015@gmail.com>
2025-03-31 18:45:05 -0700

    added xarm7

--- robosuite/robots/__init__.py
+++ robosuite/robots/__init__.py
@@ -30,6 +30,7 @@ ROBOT_CLASS_MAPPING = {
     "GR1FloatingBody": LeggedRobot,
     "PandaDexRH": FixedBaseRobot,
     "PandaDexLH": FixedBaseRobot,
+    "XArm7": FixedBaseRobot
 }
 
 target_type_mapping = {
