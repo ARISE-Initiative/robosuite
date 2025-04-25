9a17d12de5d5c4c4d662f433d9f92abb13b5ece4
snasiriany <snasiriany@gmail.com>
2025-04-24 20:21:43 -0500

    add option to enable multiccd flag programatically

--- robosuite/models/world.py
+++ robosuite/models/world.py
@@ -1,3 +1,4 @@
+import xml.etree.ElementTree as ET
 import robosuite.macros as macros
 from robosuite.models.base import MujocoXML
 from robosuite.utils.mjcf_utils import convert_to_string, find_elements, xml_path_completion
@@ -6,8 +7,16 @@ from robosuite.utils.mjcf_utils import convert_to_string, find_elements, xml_pat
 class MujocoWorldBase(MujocoXML):
     """Base class to inherit all mujoco worlds from."""
 
-    def __init__(self):
+    def __init__(self, enable_multiccd=False):
         super().__init__(xml_path_completion("base.xml"))
         # Modify the simulation timestep to be the requested value
         options = find_elements(root=self.root, tags="option", attribs=None, return_first=True)
         options.set("timestep", convert_to_string(macros.SIMULATION_TIMESTEP))
+        self.enable_multiccd = enable_multiccd
+        if self.enable_multiccd:
+            multiccd_elem = ET.fromstring(
+                """<option> <flag multiccd="enable"/> </option>"""
+            )
+            mujoco_elem = find_elements(self.root, "mujoco")
+            mujoco_elem.insert(0, multiccd_elem)
+        
