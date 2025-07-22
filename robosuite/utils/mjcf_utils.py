136ac6a8244134d742590c868a5be6a5add318f9
Abhiram824 <abhisuhaas1@gmail.com>
2025-07-22 15:20:58 -0500

    fix site scaling to get all sites from world body

--- robosuite/utils/mjcf_utils.py
+++ robosuite/utils/mjcf_utils.py
@@ -1060,7 +1060,7 @@ def scale_site_element(element, scale_array):
         element.set("size", s_size)
 
 
-def scale_mjcf_model(obj, asset_root, scale, get_elements_func, scale_slide_joints=True):
+def scale_mjcf_model(obj, asset_root, worldbody, scale, get_elements_func, scale_slide_joints=True):
     """
     Scales all elements (geoms, meshes, bodies, joints, sites) in an MJCF model.
 
@@ -1098,7 +1098,7 @@ def scale_mjcf_model(obj, asset_root, scale, get_elements_func, scale_slide_join
         scale_joint_element(elem, scale_array, scale_slide_joints)
 
     # Scale sites
-    site_pairs = get_elements_func(obj, "site")
+    site_pairs = get_elements_func(worldbody, "site")
     for (_, elem) in site_pairs:
         scale_site_element(elem, scale_array)
 
