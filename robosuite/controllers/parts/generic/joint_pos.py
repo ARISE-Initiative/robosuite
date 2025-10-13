fix torque calculation for grav comp

robosuite/controllers/parts/generic/joint_pos.py
@@ -261,11 +261,11 @@ class JointPositionController(Controller):
         vel_pos_error = -self.joint_vel
         desired_torque = np.multiply(np.array(position_error), np.array(self.kp)) + np.multiply(vel_pos_error, self.kd)
 
-        # Return desired torques plus gravity compensations
+        # Return desired torques plus gravity compensation
+        # Note: the desired torques here is actually desired accelerations
+        self.torques = np.dot(self.mass_matrix, desired_torque)
         if self.use_torque_compensation:
-            self.torques = np.dot(self.mass_matrix, desired_torque) + self.torque_compensation
-        else:
-            self.torques = desired_torque
+            self.torques += self.torque_compensation
 
         # Always run superclass call for any cleanups at the end
         super().run_controller()