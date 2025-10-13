1e55b1786df053ac5ce6dba036e6cea8731121f2
yuqix <yuqix@nvidia.com>
2025-10-13 15:46:18 -0700

    fix lint

--- robosuite/robots/robot.py
+++ robosuite/robots/robot.py
@@ -1,9 +1,9 @@
 import copy
 import json
-import mujoco
 from collections import OrderedDict
 from typing import Optional
 
+import mujoco
 import numpy as np
 
 import robosuite.utils.transform_utils as T
