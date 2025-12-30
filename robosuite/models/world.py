8da298dbdd2ee6cfee0b8e6b23a42c2050399906
Abhiram824 <abhisuhaas1@gmail.com>
2025-12-30 17:41:28 -0600

    add flag for sleeping

--- robosuite/models/world.py
+++ robosuite/models/world.py
@@ -7,16 +7,23 @@ from robosuite.utils.mjcf_utils import convert_to_string, find_elements, xml_pat
 class MujocoWorldBase(MujocoXML):
     """Base class to inherit all mujoco worlds from."""
 
-    def __init__(self, enable_multiccd=False):
+    def __init__(self, enable_multiccd=False, enable_sleeping=False):
         super().__init__(xml_path_completion("base.xml"))
         # Modify the simulation timestep to be the requested value
         options = find_elements(root=self.root, tags="option", attribs=None, return_first=True)
         options.set("timestep", convert_to_string(macros.SIMULATION_TIMESTEP))
         self.enable_multiccd = enable_multiccd
+        self.enable_sleeping = enable_sleeping
         if self.enable_multiccd:
             multiccd_elem = ET.fromstring(
                 """<option> <flag multiccd="enable"/> </option>"""
             )
             mujoco_elem = find_elements(self.root, "mujoco")
             mujoco_elem.insert(0, multiccd_elem)
+        if self.enable_sleeping:
+            sleeping_elem = ET.fromstring(
+                """<option> <flag sleep="enable"/> </option>"""
+            )
+            mujoco_elem = find_elements(self.root, "mujoco")
+            mujoco_elem.insert(0, sleeping_elem)   
         
