2471e2d3791799d8ae99dc96f28b4813ea4ab731
Abhiram824 <abhisuhaas1@gmail.com>
2026-01-20 14:55:16 -0600

    rename var

--- robosuite/models/world.py
+++ robosuite/models/world.py
@@ -7,20 +7,20 @@ from robosuite.utils.mjcf_utils import convert_to_string, find_elements, xml_pat
 class MujocoWorldBase(MujocoXML):
     """Base class to inherit all mujoco worlds from."""
 
-    def __init__(self, enable_multiccd=False, enable_sleeping=False):
+    def __init__(self, enable_multiccd=False, enable_sleeping_islands=False):
         super().__init__(xml_path_completion("base.xml"))
         # Modify the simulation timestep to be the requested value
         options = find_elements(root=self.root, tags="option", attribs=None, return_first=True)
         options.set("timestep", convert_to_string(macros.SIMULATION_TIMESTEP))
         self.enable_multiccd = enable_multiccd
-        self.enable_sleeping = enable_sleeping
+        self.enable_sleeping_islands = enable_sleeping_islands
         if self.enable_multiccd:
             multiccd_elem = ET.fromstring(
                 """<option> <flag multiccd="enable"/> </option>"""
             )
             mujoco_elem = find_elements(self.root, "mujoco")
             mujoco_elem.insert(0, multiccd_elem)
-        if self.enable_sleeping:
+        if self.enable_sleeping_islands:
             sleeping_elem = ET.fromstring(
                 """<option> <flag sleep="enable"/> </option>"""
             )
